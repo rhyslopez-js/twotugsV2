80e0e96d9fc421c1e85fa8bc08f2d2db15fd71d9
Rhys Paulino <rhys.paulino@vanarts.com>
2023-09-24 00:59:46 -0700

    nav and home drafts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
